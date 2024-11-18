58c0b77201dc03a7ca7ec3d96ffa003a0e9bc570
Jakub Skoczylas <skoczylasjakub1984@gmail.com>
2024-11-18 12:19:40 +0100

    Add project files.

---
+++ MotoApp/Program.cs
@@ -0,0 +1,7 @@
+﻿using MotoApp.Entities;
+using MotoApp.Repositories;
+var employeeRepository = new EmployeeRepository();
+employeeRepository.Add(new Employee { FirstName = "Adam" });
+employeeRepository.Add(new Employee { FirstName = "Piotr" });
+employeeRepository.Add(new Employee { FirstName = "Zuzia" });
+employeeRepository.Save();
