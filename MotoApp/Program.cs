5ee9c5f25c85b87694ebc021091731cab045ba8f
Jakub Skoczylas <skoczylasjakub1984@gmail.com>
2024-11-25 12:57:05 +0100

    Stan po usunięciu GenericRepositoryWihRemove

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿using MotoApp.Entities;
 using MotoApp.Repositories;
-var employeeRepository = new EmployeeRepository();
+var employeeRepository = new GenericRepository<Employee>();
 employeeRepository.Add(new Employee { FirstName = "Adam" });
 employeeRepository.Add(new Employee { FirstName = "Piotr" });
 employeeRepository.Add(new Employee { FirstName = "Zuzia" });
