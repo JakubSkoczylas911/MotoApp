e450526d372689a50c75e8ee00ed05b2714e0655
Jakub Skoczylas <skoczylasjakub1984@gmail.com>
2024-11-29 12:19:00 +0100

    Zmiany po wprowadzeniu Interfejsu generycznego

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿using MotoApp.Entities;
 using MotoApp.Repositories;
-var employeeRepository = new GenericRepository<Employee>();
+var employeeRepository = new ListRepository<Employee>();
 employeeRepository.Add(new Employee { FirstName = "Adam" });
 employeeRepository.Add(new Employee { FirstName = "Piotr" });
 employeeRepository.Add(new Employee { FirstName = "Zuzia" });
---
+++ MotoApp/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace MotoApp.Repositories;
+using MotoApp.Entities;
+
+public interface IRepository<T>where T:class, IEntity
+{
+    IEnumerable<T> GetAll();
+    T GetById(int id);
+    void Add(T item);
+    void Remove(T item);
+    void Save();
+}
