8d10a888a8ac6c6f6d591a40e568bcd0ec24179a
Jakub Skoczylas <skoczylasjakub1984@gmail.com>
2024-12-12 13:19:42 +0100

    Problem:(

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -1,33 +1,36 @@
 ﻿using MotoApp.Data;
 using MotoApp.Entities;
 using MotoApp.Repositories;
+using MotoApp.Repositories.Extensions;
 var employeeRepository = new SqlRepository<Employee>(new MotoAppDbContext());
 AddEmployees(employeeRepository);
 WriteAllToConsole(employeeRepository);
-static void AddEmployees(IRepository<Employee> employeeRepository)
+static void AddEmployees(IRepository<BusinessPartner> businessPartnerRepository)
 {
-    var employees = new[]
+    var bussinessPartners = new[]
     {
-        new Employee { FirstName = "Adam" },
-        new Employee { FirstName = "Piotr" },
-        new Employee { FirstName = "Zuzia" }
+        new BusinessPartner {},
+        new BusinessPartner { },
+        new BusinessPartner { },
     };
-    AddBatch(employeeRepository, employees);
-}
-static void AddBatch<T>(IRepository<T> repository, T[] items)
-    where T : class, IEntity
-{
-    foreach (var employee in items)
+    businessPartnerRepository.AddBatch(bussinessPartners);
+    //    AddBatch(employeeRepository, employees);
+    //}
+    //static void AddBatch<T>(IRepository<T> repository, T[] items)
+    //    where T : class, IEntity
+    //{
+    //    foreach (var item in items)
+    //    {
+    //        repository.Add(item);
+    //    }
+    //    repository.Save();
+    //}
+    static void WriteAllToConsole(IReadRepository<IEntity> repository)
     {
-        repository.Add(employee);
-    }
-    repository.Save();
-}
-static void WriteAllToConsole(IReadRepository<IEntity> repository)
-{
-    var items = repository.GetAll();
-    foreach (var item in items)
-    {
-        Console.WriteLine(item);
+        var items = repository.GetAll();
+        foreach (var item in items)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
