f1d2973b41bfd665db16ee30bb83b9ca1b5fcc82
Jakub Skoczylas <skoczylasjakub1984@gmail.com>
2024-12-18 12:14:24 +0100

    Działający Program.cs:)

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -2,10 +2,16 @@
 using MotoApp.Entities;
 using MotoApp.Repositories;
 using MotoApp.Repositories.Extensions;
+var itemAdded = new ItemAdded(EmployeeAdded);
 
-var employeeRepository = new SqlRepository<Employee>(new MotoAppDbContext());
+var employeeRepository = new SqlRepository<Employee>(new MotoAppDbContext(), itemAdded);
 AddEmployees(employeeRepository);
 WriteAllToConsole(employeeRepository);
+static void EmployeeAdded(object item)
+{
+    var employee = (Employee)item;
+    Console.WriteLine($"{employee.FirstName}added");
+}
 static void AddEmployees(IRepository<Employee> repository)
 {
     var employees = new[]
--- MotoApp/Repositories/SqlRepository.cs
+++ MotoApp/Repositories/SqlRepository.cs
@@ -1,16 +1,18 @@
 ﻿namespace MotoApp.Repositories;
 using Microsoft.EntityFrameworkCore;
 using MotoApp.Entities;
-
+public delegate void ItemAdded(object item);
 
 public class SqlRepository<T> : IRepository<T> where T : class, IEntity, new()
 {
     private readonly DbSet<T> _dbSet;
     private readonly DbContext _dbContext;
-    public SqlRepository(DbContext dbContext)
+    private readonly ItemAdded? _itemAddedCallback;
+    public SqlRepository(DbContext dbContext, ItemAdded? itemAddedCallback = null)
     {
         _dbContext = dbContext;
         _dbSet = _dbContext.Set<T>();
+        _itemAddedCallback = itemAddedCallback;
     }
     public IEnumerable<T> GetAll()
     {
@@ -24,6 +26,7 @@ public class SqlRepository<T> : IRepository<T> where T : class, IEntity, new()
     public void Add(T item)
     {
         _dbSet.Add(item);
+        _itemAddedCallback?.Invoke(item);
     }
     public void Remove(T item)
     {
