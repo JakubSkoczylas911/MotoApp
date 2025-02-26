f88a295bd3a6cd20c6c002cfbe0d0a453acb7ec0
Jakub Skoczylas <skoczylasjakub1984@gmail.com>
2025-02-26 16:52:25 +0100

    Najnowszy, bezbłędny,sformatowany program

---
+++ MotoApp/Components/CsvReader/ICsvReader.cs
@@ -0,0 +1,9 @@
+﻿namespace MotoApp.Components.CsvReader;
+using MotoApp.Components.CsvReader.Extensions;
+using MotoApp.Components.CsvReader.Models;
+public interface ICsvReader
+{
+    List<Car> ProcessCars(string filePath);
+    List<Manufacturer> ProcessManufactures(string filePath);
+
+}
