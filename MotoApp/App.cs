a914fd028ce697459a5f99d1d79f961c95b7b9e9
Jakub Skoczylas <skoczylasjakub1984@gmail.com>
2025-03-05 11:50:28 +0100

    Działający,Najnowszy(po wprowadzeniuJoin), bezbłędny,sformatowany program:)

--- MotoApp/App.cs
+++ MotoApp/App.cs
@@ -11,23 +11,38 @@ public class App : IApp
     {
         var cars = _csvReader.ProcessCars("Resources\\Files\\fuel.csv");
         var manufacturers = _csvReader.ProcessManufactures("Resources\\Files\\manufacturers.csv");
-        var groups = cars
-            .GroupBy(x => x.Manufacturer)
-            .Select(g => new
+        //var groups = cars
+        //    .GroupBy(x => x.Manufacturer)
+        //    .Select(g => new
+        //    {
+        //        Name = g.Key,
+        //        Max = g.Max(cars => cars.Combined),
+        //        Average = g.Average(c => c.Combined)
+        //    })
+        //    .OrderBy(x => x.Average);
+        //foreach (var group in groups)
+        //{
+        //    Console.WriteLine($"{group.Name}");
+        //    Console.WriteLine($"Max:{group.Max}");
+        //    Console.WriteLine($"Average:{group.Average}");
+        //}
+        var carsInCountry = cars.Join(manufacturers, x => x.Manufacturer, x => x.Name,
+            (car, manufacturer) =>
+            new
             {
-                Name = g.Key,
-                Max = g.Max(cars => cars.Combined),
-                Average = g.Average(c => c.Combined)
+                manufacturer.Country,
+                car.Name,
+                car.Combined
             })
-            .OrderBy(x => x.Average);
-        foreach (var group in groups)
+            .OrderByDescending(x => x.Combined)
+            .ThenBy(x => x.Name);
+        foreach (var car in carsInCountry)
         {
-            Console.WriteLine($"{group.Name}");
-            Console.WriteLine($"Max:{group.Max}");
-            Console.WriteLine($"Average:{group.Average}");
+            Console.WriteLine($"Country:{car.Country}");
+            Console.WriteLine($"Name:{car.Name}");
+            Console.WriteLine($"Combined:{car.Combined}");
         }
     }
 }
-    
 
-    
+
