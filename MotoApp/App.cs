c5bb18da047d7f79c55f5b26e8f528401b090050
Jakub Skoczylas <skoczylasjakub1984@gmail.com>
2025-03-08 10:24:15 +0100

    Działający,Najnowszy(po wprowadzeniu Group Join), bezbłędny,sformatowany,program:)

--- MotoApp/App.cs
+++ MotoApp/App.cs
@@ -13,40 +13,62 @@ public class App : IApp
     {
         var cars = _csvReader.ProcessCars("Resources\\Files\\fuel.csv");
         var manufacturers = _csvReader.ProcessManufactures("Resources\\Files\\manufacturers.csv");
-        //var groups = cars
-        //    .GroupBy(x => x.Manufacturer)
-        //    .Select(g => new
-        //    {
-        //        Name = g.Key,
-        //        Max = g.Max(cars => cars.Combined),
-        //        Average = g.Average(c => c.Combined)
-        //    })
-        //    .OrderBy(x => x.Average);
-        //foreach (var group in groups)
-        //{
-        //    Console.WriteLine($"{group.Name}");
-        //    Console.WriteLine($"Max:{group.Max}");
-        //    Console.WriteLine($"Average:{group.Average}");
-        //}
-        var carsInCountry = cars.Join(manufacturers,
-            c => new { c.Manufacturer, c.Year },
-            m => new { Manufacturer = m.Name, m.Year },
-            (car, manufacturer) =>
+        var groups = manufacturers.GroupJoin(
+            cars,
+            manufacturer => manufacturer.Name,
+            car => car.Manufacturer,
+            (m, g) =>
             new
             {
-                manufacturer.Country,
-                car.Name,
-                car.Combined
+                Manufacturer = m,
+                Cars = g
             })
-            .OrderByDescending(x => x.Combined)
-            .ThenBy(x => x.Name);
-        foreach (var car in carsInCountry)
+            .OrderBy(x => x.Manufacturer.Name);
+        foreach (var car in groups)
         {
-            Console.WriteLine($"Country:{car.Country}");
-            Console.WriteLine($"Name:{car.Name}");
-            Console.WriteLine($"Combined:{car.Combined}");
+            Console.WriteLine($"Manufacturer:{car.Manufacturer.Name}");
+            Console.WriteLine($"Cars:{car.Cars.Count()}");
+            Console.WriteLine($"Max:{car.Cars.Max(x => x.Combined)}");
+            Console.WriteLine($"Min:{car.Cars.Min(x => x.Combined)}");
+            Console.WriteLine($"Avg:{car.Cars.Average(x => x.Combined)}");
+            Console.WriteLine();
         }
     }
 }
+
+//    .GroupBy(x => x.Manufacturer)
+//    .Select(g => new
+//    {
+//        Name = g.Key,
+//        Max = g.Max(cars => cars.Combined),
+//        Average = g.Average(c => c.Combined)
+//    })
+//    .OrderBy(x => x.Average);
+//foreach (var group in groups)
+//{
+//    Console.WriteLine($"{group.Name}");
+//    Console.WriteLine($"Max:{group.Max}");
+//    Console.WriteLine($"Average:{group.Average}");
+//}
+//        var carsInCountry = cars.Join(manufacturers,
+//            c => new { c.Manufacturer, c.Year },
+//            m => new { Manufacturer = m.Name, m.Year },
+//            (car, manufacturer) =>
+//            new
+//            {
+//                manufacturer.Country,
+//                car.Name,
+//                car.Combined
+//            })
+//            .OrderByDescending(x => x.Combined)
+//            .ThenBy(x => x.Name);
+//        foreach (var car in carsInCountry)
+//        {
+//            Console.WriteLine($"Country:{car.Country}");
+//            Console.WriteLine($"Name:{car.Name}");
+//            Console.WriteLine($"Combined:{car.Combined}");
+//        }
+//    }
+//}
 
 
